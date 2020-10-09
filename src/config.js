61285822162a6455c56e1f2299ec98f5ced7ac67
Weslley Cavalcante <whenriquerc@gmail.com>
2020-10-09 17:59:19 -0300

    feat: add gtm config

---
+++ src/config.js
@@ -0,0 +1,11 @@
+export const prod = {
+  gtmId: 'GTM-M8HBZ6Z',
+  auth: 'po1Airv4VOoeF0MEFy5jTQ',
+  preview: 'env-1'
+}
+
+export const stg = {
+  gtmId: 'GTM-M8HBZ6Z',
+  auth: 'SoaTTDNLJ0xtf7Ij6vFW4g',
+  preview: 'env-3'
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import TagManager from 'react-gtm-module'
 import App from './App';
+import { prod } from './config';
 import './style.css'
+
+TagManager.initialize(prod)
 
 ReactDOM.render(
   <App />,
